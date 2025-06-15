d36a3bd2191eaaa6dfd003c80d4acd99146f54a8
Jigar Prajapati <jigarprajapati6457@gmail.com>
2025-06-16 01:04:39 +0530

    change in input section

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
-
-// components/SearchBar.jsx
 function SearchBar({ search, setSearch, fetchWeather }) {
   return (
-    <div className="flex bg-white rounded flex-col sm:flex-row justify-between items-center gap-4 mb-6 ml-2">
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search city..."
-        className="w-full sm:flex-1 px-4 py-2 rounded-lg text-black"
-      />
-      <button
-        onClick={() => fetchWeather(search)}
-        className="bg-black text-white px-6 py-2 rounded-lg font-semibold w-full sm:w-auto"
-      >
-        Search
-      </button>
+    <div className="flex justify-center mb-6">
+      <div className="flex w-full max-w-md">
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          placeholder="Search city..."
+          className="flex-1 bg-white  py-2 pl-3 text-black border border-black rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          onClick={() => fetchWeather(search)}
+          className="bg-black text-white px-6 py-2 rounded-r-md font-semibold border border-black"
+        >
+          Search
+        </button>
+      </div>
     </div>
   );
 }
